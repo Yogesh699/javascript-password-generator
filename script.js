e7a8a3ceabe0238eb4882d2c9d8662e50425833f
prework <143371945+Yogesh699@users.noreply.github.com>
2023-10-02 21:05:07 +1030

    prompted for password length

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {  
+  console.log("generate password") 
+  
+  let length=parseInt(prompt("provide password link between 8 and 128"));
+  if(isNaN(length)){
+    return("length must be a number, please retry");
+  }
+
+  if(length < 8 || length > 128){
+    return("length must be between 8 and 128, please retry");
+  }
+
+  
+}
